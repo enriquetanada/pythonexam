41cf05ad8b2a960fb68b4c72b0dd5253d091f29c
enriquetanada <enriquetanada@gmail.com>
2023-12-11 21:41:23 +0800

    create age field and compute age

--- age.py
+++ age.py
@@ -1,3 +1,5 @@
+from datetime import date
+import datetime
 import requests
 
 fieldnames = ["name", "birthday", "age"]
@@ -5,5 +7,14 @@ fieldnames = ["name", "birthday", "age"]
 response = requests.get("https://tr148rto1k.execute-api.ap-southeast-2.amazonaws.com/dev/birthdays")
 
 data = response.json()
+
+today = date.today()
+
+for person in (data["body"]):
+    birthDate = datetime.datetime.strptime(person["birthday"], '%Y-%m-%d')
+
+    compareMonthAndDay = ((today.month, today.day) < (birthDate.month, birthDate.day))
+
+    person["age"] = today.year - birthDate.year - compareMonthAndDay
 
 print(data)
